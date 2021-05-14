48390343900977f3289a83892c73a82c3d8e84f0
Maciej Gajewski <maciej.gajewski0@gmail.com>
2021-05-14 20:53:52 +0200

    qualified name tests

--- utils/tests/qualified_name_tests.cc
+++ utils/tests/qualified_name_tests.cc
@@ -11,6 +11,14 @@ TEST(QualifiedNameTests, Formatting) {
   QualifiedName qn("abc");
 
   EXPECT_EQ("abc", fmt::format("{}", qn));
+
+  qn.appendScope("s1");
+
+  EXPECT_EQ("s1::abc", fmt::format("{}", qn));
+
+  qn.appendScope("s2");
+
+  EXPECT_EQ("s1::s2::abc", fmt::format("{}", qn));
 }
 
 } // namespace Cougar::Utils::Tests
